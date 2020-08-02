2f6cb19b0679c9a468acab566521a18f408163e2
Smruti Ranjan Panda <smpanda@microsoft.com>
2020-08-02 23:07:40 +0530

    CosmosDB

---
+++ COSMOSDB.SDK/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+
+namespace COSMOSDB.SDK
+{
+    class Program
+    {
+        static async  Task Main(string[] args)
+        {
+            var endPoint = "https://localhost:8081";
+            var key = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
+
+            var client = new DocumentClient(new Uri(endPoint), key);
+
+            var databaseDefination = new Database { Id = "vsdb" };
+            await client.CreateDatabaseAsync(databaseDefination);
+
+        }
+    }
+}
